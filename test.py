7d536bd916dd1441db063aa4ef2b4c94550ce541
JiJimishelle <162943492+JiJimishelle@users.noreply.github.com>
2024-10-15 00:27:08 +0900

    Source_code.py

--- test.py
+++ test.py
@@ -62,3 +62,51 @@ print('x_train:\n', x_train,)
 print('x_test:\n', x_test)
 print('y_train:\n', y_train)
 print('y_test:\n', y_test)
+
+
+
+#--------------------------------------------------------------------------------------------------------------------------
+
+import numpy as np 
+import pandas as pd
+data_set= pd.read_csv('/Users/jiji/Desktop/Study/ML/Data.csv')       
+x= data_set.iloc[:,:-1].values
+y= data_set.iloc[:,3].values
+
+from sklearn.impute import SimpleImputer
+imputer= SimpleImputer(missing_values=np.nan, strategy='mean')
+x[:,1:3]= imputer.fit_transform(x[:,1:3])
+
+from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+x[:,0]=LabelEncoder().fit_transform(x[:,0])
+
+from sklearn.compose import ColumnTransformer
+ct= ColumnTransformer(transformers=[('onehot', OneHotEncoder(), [0])],remainder='passthrough')
+x= ct.fit_transform(x)
+
+from sklearn.model_selection import train_test_split
+x_train, x_test, y_train, y_test= train_test_split(x, y, test_size= 0.2, random_state= 42)
+
+print('x_train:\n', x_train,)
+print('x_test:\n', x_test)
+print('y_train:\n', y_train)
+print('y_test:\n', y_test)
+
+#-----------OUTPUT----------
+
+x_train:
+ [[1.0 0.0 0.0 35.0 58000.0]
+ [1.0 0.0 0.0 44.0 72000.0]
+ [1.0 0.0 0.0 48.0 79000.0]
+ [0.0 1.0 0.0 30.0 54000.0]
+ [1.0 0.0 0.0 37.0 67000.0]
+ [0.0 1.0 0.0 40.0 63777.77777777778]
+ [0.0 0.0 1.0 38.0 61000.0]
+ [0.0 0.0 1.0 38.77777777777778 52000.0]]
+x_test:
+ [[0.0 1.0 0.0 50.0 83000.0]
+ [0.0 0.0 1.0 27.0 48000.0]]
+y_train:
+ ['Yes' 'No' 'Yes' 'No' 'Yes' 'Yes' 'No' 'No']
+y_test:
+ ['No' 'Yes']
